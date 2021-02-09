67ea58c5a0df961551e93cf3d93d019c91587e0b
bebebe <garasum@ukr.net>
2021-02-09 17:34:20 +0200

    add oms2

---
+++ src/test/java/com/softserveinc/edu/App.java
@@ -0,0 +1,15 @@
+package com.softserveinc.edu;
+
+import org.junit.Test;
+
+public class App {
+
+	@Test
+    public void checkApp() {
+        // From OS
+        System.out.println("***System.getenv(\"MY_PASSWORD\") = " + System.getenv("MY_PASSWORD"));
+        System.out.println("***System.getenv(\"DEFAULT_PASS\") = " + System.getenv("DEFAULT_PASS"));
+        // From Eclipse/Idea
+        System.out.println("***System.getenv().MY_IDE = " + System.getenv().get("MY_IDE"));
+    }
+}
